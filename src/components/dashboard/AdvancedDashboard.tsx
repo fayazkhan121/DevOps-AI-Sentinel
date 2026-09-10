e6bcb88f284f9061f38087bc17271884eace7e2e
Fayaz Khan <infomaliks7@gmail.com>
2026-09-10 07:33:07 +0000

    fix: rename integration test binding and restore dashboard JSX
    Signed-off-by: Fayaz Khan <infomaliks7@gmail.com>

--- src/components/dashboard/AdvancedDashboard.tsx
+++ src/components/dashboard/AdvancedDashboard.tsx
@@ -313,8 +313,6 @@ export const AdvancedDashboard: React.FC = () => {
           </CardContent>
         </Card>
 
-        </Card>
-
         {metrics.cost.currentPeriod > 0 && (
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
